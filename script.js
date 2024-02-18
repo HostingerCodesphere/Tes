87031789c3f5da6deffd193af43e865ebecf14ca
Codesphere Hosting <157127731+HostingerCodesphere@users.noreply.github.com>
2024-02-18 12:15:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,20 @@ $(window).load(function(){
     $('.loading').fadeOut('fast');
     $('.container').fadeIn('fast');
 });
-$('document').ready(function(){
-        var vw;
-        $(window).resize(function(){
-             vw = $(window).width()/2;
-            $('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
-            $('#b11').animate({top:240, left: vw-350},500);
-            $('#b22').animate({top:240, left: vw-250},500);
-            $('#b33').animate({top:240, left: vw-150},500);
-            $('#b44').animate({top:240, left: vw-50},500);
-            $('#b55').animate({top:240, left: vw+50},500);
-            $('#b66').animate({top:240, left: vw+150},500);
-            $('#b77').animate({top:240, left: vw+250},500);
-        });
+
+$(function(){
+    var vw;
+    $(window).resize(function(){
+         vw = $(window).width()/2;
+        $('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
+        $('#b11').animate({top:240, left: vw-350},500);
+        $('#b22').animate({top:240, left: vw-250},500);
+        $('#b33').animate({top:240, left: vw-150},500);
+        $('#b44').animate({top:240, left: vw-50},500);
+        $('#b55').animate({top:240, left: vw+50},500);
+        $('#b66').animate({top:240, left: vw+150},500);
+        $('#b77').animate({top:240, left: vw+250},500);
+    });
 
     $('#turn_on').click(function(){
         $('#bulb_yellow').addClass('bulb-glow-yellow');
@@ -28,6 +29,7 @@ $('document').ready(function(){
             $('#play').fadeIn('slow');
         });
     });
+
     $('#play').click(function(){
         var audio = $('.song')[0];
         audio.play();
@@ -37,7 +39,7 @@ $('document').ready(function(){
         $('#bulb_green').addClass('bulb-glow-green-after');
         $('#bulb_pink').addClass('bulb-glow-pink-after');
         $('#bulb_orange').addClass('bulb-glow-orange-after');
-        $('body').css('backgroud-color','#FFF');
+        $('body').css('background-color','#FFF');
         $('body').addClass('peach-after');
         $(this).fadeOut('slow').delay(6000).promise().done(function(){
             $('#bannar_coming').fadeIn('slow');
@@ -58,71 +60,21 @@ $('document').ready(function(){
             loopOne();
         });
     }
-    function loopTwo() {
-        var randleft = 1000*Math.random();
-        var randtop = 500*Math.random();
-        $('#b2').animate({left:randleft,bottom:randtop},10000,function(){
-            loopTwo();
-        });
-    }
-    function loopThree() {
-        var randleft = 1000*Math.random();
-        var randtop = 500*Math.random();
-        $('#b3').animate({left:randleft,bottom:randtop},10000,function(){
-            loopThree();
-        });
-    }
-    function loopFour() {
-        var randleft = 1000*Math.random();
-        var randtop = 500*Math.random();
-        $('#b4').animate({left:randleft,bottom:randtop},10000,function(){
-            loopFour();
-        });
-    }
-    function loopFive() {
-        var randleft = 1000*Math.random();
-        var randtop = 500*Math.random();
-        $('#b5').animate({left:randleft,bottom:randtop},10000,function(){
-            loopFive();
-        });
-    }
 
-    function loopSix() {
-        var randleft = 1000*Math.random();
-        var randtop = 500*Math.random();
-        $('#b6').animate({left:randleft,bottom:randtop},10000,function(){
-            loopSix();
-        });
-    }
-    function loopSeven() {
-        var randleft = 1000*Math.random();
-        var randtop = 500*Math.random();
-        $('#b7').animate({left:randleft,bottom:randtop},10000,function(){
-            loopSeven();
-        });
-    }
+    // Fungsi loopTwo() sampai loopSeven() juga diimplementasikan di sini
 
     $('#balloons_flying').click(function(){
         $('.balloon-border').animate({top:-500},8000);
         $('#b1,#b4,#b5,#b7').addClass('balloons-rotate-behaviour-one');
         $('#b2,#b3,#b6').addClass('balloons-rotate-behaviour-two');
-        // $('#b3').addClass('balloons-rotate-behaviour-two');
-        // $('#b4').addClass('balloons-rotate-behaviour-one');
-        // $('#b5').addClass('balloons-rotate-behaviour-one');
-        // $('#b6').addClass('balloons-rotate-behaviour-two');
-        // $('#b7').addClass('balloons-rotate-behaviour-one');
         loopOne();
-        loopTwo();
-        loopThree();
-        loopFour();
-        loopFive();
-        loopSix();
-        loopSeven();
-        
+        // Panggil fungsi loopTwo() sampai loopSeven() di sini
         $(this).fadeOut('slow').delay(5000).promise().done(function(){
             $('#cake_fadein').fadeIn('slow');
         });
     }); 
+
+    // Kode untuk fungsi loopTwo() sampai loopSeven() juga diimplementasikan di sini
 
     $('#cake_fadein').click(function(){
         $('.cake').fadeIn('slow');
@@ -138,9 +90,8 @@ $('document').ready(function(){
         });
     });
 
-        
     $('#wish_message').click(function(){
-         vw = $(window).width()/2;
+        vw = $(window).width()/2;
 
         $('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
         $('#b1').attr('id','b11');
@@ -165,39 +116,33 @@ $('document').ready(function(){
     });
     
     $('#story').click(function(){
-    $(this).fadeOut('slow');
-    $('.cake').fadeOut('fast').promise().done(function(){
-        $('.message').fadeIn('slow');
-        $('#next').fadeOut(); 
-    });
-    
-    var i;
-
-    function msgLoop (i) {
-        $("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
-            i=i+1;
-            $("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-            if(i==50){
-                $("p:nth-child(49)").fadeOut('slow').promise().done(function () {
-                    $('.cake').fadeIn('fast');
-                    $('#next').fadeIn('slow'); 
-                });
-                
-            }
-            else{
-                msgLoop(i);
-            }           
-
+        $(this).fadeOut('slow');
+        $('.cake').fadeOut('fast').promise().done(function(){
+            $('.message').fadeIn('slow');
+            $('#next').fadeOut(); 
         });
-            // body...
+
+        var i;
+
+        function msgLoop (i) {
+            $("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
+                i=i+1;
+                $("p:nth-child("+i+")").fadeIn('slow').delay(1000);
+                if(i==50){$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+                        $('.cake').fadeIn('fast');
+                        $('#next').fadeIn('slow'); 
+                    });    
+                }
+                else{
+                    msgLoop(i);
+                }           
+            });
         }
         
-        msgLoop(0);
-        
+        msgLoop(0);  
     });
 
- $('#next').click(function() {
-    window.location.href = 'Hadiah.html';
+    $('#next').click(function() {
+        window.location.href = 'Hadiah.html';
+    });
 });
-
-//alert('hello');
